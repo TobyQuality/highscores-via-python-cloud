5c6d851ec44c93b8f217800acf0c70f093d1b4ec
Toby Quality <topias.laatu@gmail.com>
2023-04-26 11:48:02 +0300

    Changed the key's name from 'name' to 'player_name' in add_highscore function. Also made more tests for functions

--- index.py
+++ index.py
@@ -16,6 +16,18 @@ API_KEY = os.environ.get('API_KEY')
 # this method is used to check the validity of the password
 # sent with requests to the backend
 def check_api_key(pw):
+    """
+    Check the validity of the password sent with requests to the backend.
+
+    Args:
+        pw (str): The password string to be checked.
+
+    Returns:
+        bool: True if the password is valid, False otherwise.
+
+    Raises:
+        None
+    """
     # hashing the API_KEY that has been turned to bytes array
     # with randomly generated salt
     hash = bcrypt.hashpw(API_KEY.encode('utf-8'), bcrypt.gensalt())
@@ -26,10 +38,28 @@ def check_api_key(pw):
 
 @app.route("/")
 def root():
-    return "<h1>hello</h1>"
+    """
+    Route for the root URL ("/").
+
+    Returns:
+        str: A string with a greeting message.
+
+    Raises:
+        None
+    """
+    return "<h1>:)</h1>"
 
 @app.route("/api/highscores")
 def get_highscores():
+    """
+    Route for getting highscores via API.
+
+    Returns:
+        Response: A Flask Response object containing highscores data or an error message.
+
+    Raises:
+        None
+    """
     pw = request.args.get("pw")
     if (check_api_key(pw) == False):
         return make_response(jsonify("You are unauthorized"), 401)
@@ -38,6 +68,18 @@ def get_highscores():
 
 @app.route('/api/highscores/<int:the_id>')
 def get_players_highscore(the_id):
+    """
+    Route for getting a specific player's highscore via API.
+
+    Args:
+        the_id (int): The ID of the player.
+
+    Returns:
+        Response: A Flask Response object containing the player's highscore data or an error message.
+
+    Raises:
+        NotFound: If the player with the given ID is not found.
+    """
     pw = request.args.get("pw")
     if (check_api_key(pw) == False):
         return make_response(jsonify("You are unauthorized"), 401)
@@ -55,18 +97,37 @@ def get_players_highscore(the_id):
 # one of the four levels of the game
 @app.route("/api/highscores", methods=['POST'])
 def add_highscore():
+    """
+    Route for adding a player's highscore via API.
+
+    Returns:
+        Response: A Flask Response object indicating success or failure of the request.
+
+    Raises:
+        None
+    """
     pw = request.args.get("pw")
     if (check_api_key(pw) == False):
         return make_response(jsonify("You are unauthorized"), 401)
     # the data sent via post request is e.g. {'name': 'dummy', 'overall_highscore': 100}
     sent_data = json.loads(request.data)
-    if validate_name(sent_data['name']) != True:
-        return make_response("Name must be be at least 5 characters and max 50 characters long", 400)
-    save_highscore(sent_data['name'], sent_data['overall_highscore'])
+    if validate_name(sent_data['player_name']) != True:
+        return make_response("Name must be be at least 3 characters and max 20 characters long", 400)
+    save_highscore(sent_data['player_name'], sent_data['overall_highscore'])
     return make_response("", 201)
 
 @app.route('/api/highscores/<int:the_id>', methods=['DELETE'])
 def delete_highscore(the_id):
+    """
+    Deletes a highscore entry for a player with the given ID.
+
+    Args:
+        the_id (int): The ID of the player whose highscore is to be deleted.
+
+    Returns:
+        Response: A Flask response object with a 204 status code on successful deletion,
+        or a response object with 404 status code if the player ID is not found.
+    """
     pw = request.args.get("pw")
     if (check_api_key(pw) == False):
         return make_response(jsonify("You are unauthorized"), 401)
@@ -81,6 +142,13 @@ def delete_highscore(the_id):
 
 @app.route('/highscores')
 def show_highscores():
+    """
+    Renders the highscores page with optional sorting and limiting options.
+
+    Returns:
+        Response: A Flask response object containing the rendered highscores page
+        with optional sorting and limiting options applied.
+    """
     # The highscore page doesn't need password
     limit = 100
     sort = "desc"
--- test/test_repository.py
+++ test/test_repository.py
@@ -1,60 +1,99 @@
-import os
 import unittest
-from unittest.mock import mock_open, patch
 from utils.repository import *
+from index import *
 
-class TestRepositoryFunctions(unittest.TestCase):
+class RepositoryTests(unittest.TestCase):
 
-    def test_open_file(self):
-        # Mocking the open function using mock_open
-        m = mock_open()
-        with patch('builtins.open', m):
-            # Call the function with 'r' argument
-            open_file('r')
-            # Assert that open was called with the correct arguments
-            m.assert_called_once_with(os.path.join('..', 'highscores.json'), 'r')
+    load_dotenv()
+    API_KEY = os.environ.get('API_KEY')
 
-    def test_read_database(self):
-        # Mocking the read_file function and its return value
-        with patch('utils.repository.read_file') as mock_read_file:
-            mock_read_file.return_value = '{"name": "John", "highscore": 100}'
-            result = read_database()
-            # Assert that the mocked read_file function was called
-            mock_read_file.assert_called_once()
-            # Assert that the result is correct
-            self.assertEqual(result, '{"name": "John", "highscore": 100}')
+    # There should be only one object inside the highscores.json
+    # Otherwise the code won't work
 
     def test_fetch_highscores(self):
-        # Mocking the read_database function and its return value
-        with patch('utils.repository.read_database') as mock_read_database:
-            mock_read_database.return_value = '[{"name": "John", "highscore": 100}, {"name": "Alice", "highscore": 90}]'
-            # Test case 1: Sort by none
-            result = fetch_highscores(limit=2, sort='none')
-            expected = [{'player': 'John', 'highscore': 100}, {'player': 'Alice', 'highscore': 90}]
-            self.assertEqual(result, expected)
+        # Test fetch_highscores function
+        highscores = fetch_highscores(limit=5, sort="desc")
+        self.assertIsInstance(highscores, list)
+        self.assertGreaterEqual(len(highscores), 0)
+        self.assertLessEqual(len(highscores), 5)
 
-            # Test case 2: Sort by asc
-            result = fetch_highscores(limit=2, sort='asc')
-            expected = [{'player': 'Alice', 'highscore': 90}, {'player': 'John', 'highscore': 100}]
-            self.assertEqual(result, expected)
+    def test_fetch_highscore(self):
+        # Test fetch_highscore function
+        player_data = fetch_highscore(1)
+        self.assertIsInstance(player_data, dict)
+        self.assertIn('id', player_data)
+        self.assertIn('name', player_data)
+        self.assertIn('highscore', player_data)
 
-            # Test case 3: Sort by desc
-            result = fetch_highscores(limit=2, sort='desc')
-            expected = [{'player': 'John', 'highscore': 100}, {'player': 'Alice', 'highscore': 90}]
-            self.assertEqual(result, expected)
+    def test_save_highscore(self):
+        # Test save_highscore function
+        save_highscore("John", 100)
+        player_data = fetch_highscore(2)
+        self.assertEqual(player_data['name'], 'John')
+        self.assertEqual(player_data['highscore'], 100)
 
-    def test_fetch_player_data(self):
-        # Mocking the read_database function and its return value
-        with patch('utils.repository.read_database') as mock_read_database:
-            mock_read_database.return_value = '[{"id": 1, "name": "John", "highscore": 100}, {"id": 2, "name": "Alice", "highscore": 90}]'
-            # Test case 1: Existing player
-            result = fetch_player_data(id=1)
-            expected = {"id": 1, "name": "John", "highscore": 100}
-            self.assertEqual(result, expected)
+    def test_delete_highscore(self):
+        #Test remove_highscore function
+        remove_highscore(2)
+        player_data = fetch_highscore(2)
+        self.assertEqual(player_data, None)
 
-            # Test case 2: Non-existing player
-            result = fetch_player_data(id=3)
-            self.assertIsNone(result)
+    def test_save_highscore_with_invalid_input(self):
+        # Test save_highscore function with invalid input
+        with self.assertRaises(Exception):
+            save_highscore("John", "invalid_score")
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_check_api_key(self):
+        # Test case 1: Correct password
+        pw = API_KEY
+        assert check_api_key(pw) == True
+
+        # Test case 2: Incorrect password
+        pw = "wrong_password"
+        assert check_api_key(pw) == False
+    
+    def test_get_highscores(self):
+        # Test case 1: Correct API key
+        with app.test_client() as client:
+            response = client.get('/api/highscores?pw=' + API_KEY)
+            assert response.status_code == 200
+
+        # Test case 2: Incorrect API key
+        with app.test_client() as client:
+            response = client.get('/api/highscores?pw=wrong_password')
+            assert response.status_code == 401
+
+    def test_get_players_highscore(self):
+        # Test case 1: Correct API key and existing player ID
+        with app.test_client() as client:
+            response = client.get('/api/highscores/1?pw=' + API_KEY)
+            assert response.status_code == 200
+
+        # Test case 2: Incorrect API key
+        with app.test_client() as client:
+            response = client.get('/api/highscores/1?pw=wrong_password')
+            assert response.status_code == 401
+
+        # Test case 3: Non-existing player ID
+        with app.test_client() as client:
+            response = client.get('/api/highscores/999?pw=' + API_KEY)
+            assert response.status_code == 404
+
+    def test_add_highscore(self):
+    # Test case 1: Correct API key and valid data
+        with app.test_client() as client:
+            sent_data = {'name': 'chummy', 'overall_highscore': 100}
+            response = client.post('/api/highscores?pw=' + API_KEY, data=json.dumps(sent_data), content_type='application/json')
+            assert response.status_code == 201
+
+        # Test case 2: Incorrect API key
+        with app.test_client() as client:
+            sent_data = {'name': 'dummy', 'overall_highscore': 100}
+            response = client.post('/api/highscores?pw=wrong_password', data=json.dumps(sent_data), content_type='application/json')
+            assert response.status_code == 401
+
+        # Test case 3: Invalid data
+        with app.test_client() as client:
+            sent_data = {'name': 'd', 'overall_highscore': 100}
+            response = client.post('/api/highscores?pw=' + API_KEY, data=json.dumps(sent_data), content_type='application/json')
+            assert response.status_code == 400
